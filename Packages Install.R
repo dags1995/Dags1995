320d62d0da2cad90788a53ac41fa68b540a02471
Daniel Garcia Sanchez <dgs1452@gmail.com>
2023-10-31 10:15:28 -0500

    Update

--- Packages Install.R
+++ Packages Install.R
@@ -3,7 +3,10 @@
 install.packages(c("ggplot2", "dplyr", "tidyr", "tidyverse", "gtools", "dslabs", 
                    "HistData", "pdftools", "gutenberg", "gam", "purrr", "Seurat",
                    "RColorBrewer", "ggrepel", "Rqc", "QuasR", "pheatmap", "DOSE",
-                   "enrichplot", "ggupset", "markdown", "knitr"))
+                   "enrichplot", "ggupset", "markdown", "knitr", "tinytex"))
+
+#Complementary for the tinytext package:
+tinytex::install_tinytex()
 
 
 # Instalation of Bioconductor packages, first:
