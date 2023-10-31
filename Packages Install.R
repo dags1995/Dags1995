4e7f2153daef0159938f2ac843baa1317aa978a1
Daniel Garcia Sanchez <dgs1452@gmail.com>
2023-10-31 09:43:30 -0500

    Actlualization of R packages list

--- Packages Install.R
+++ Packages Install.R
@@ -1,13 +1,17 @@
 ### Code to install main packages in a new PC
 
-install.packages(c("ggplot2", "dplyr", "tidyr", "tidyverse", "gtools"))
+install.packages(c("ggplot2", "dplyr", "tidyr", "tidyverse", "gtools", "dslabs", 
+                   "HistData", "pdftools", "gutenberg", "gam", "purrr", "Seurat",
+                   "RColorBrewer", "ggrepel", "Rqc", "QuasR", "pheatmap", "DOSE",
+                   "enrichplot", "ggupset", ))
 
 
 # Instalation of Bioconductor packages, first:
 if (!require("BiocManager", quietly = TRUE))
   install.packages("BiocManager")
 #Bioconductor packages
-BiocManager::install(c("clusterProfiler", "AnnotationDbi", "org.Hs.eg.db", "org.Mm.eg.db"))
+BiocManager::install(c("clusterProfiler", "AnnotationDbi", "org.Hs.eg.db", 
+                       "org.Mm.eg.db", "fgsea", ))
 
 
 
